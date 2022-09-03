795c39ffad68a8f6514e9fd67bce453f4a9f1aa0
hokyun <h0kyungim@gmail.com>
2022-09-03 11:16:15 +0900

    inital commit

---
+++ 김호균/09.02/42576.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion)
+{
+  string answer = "";
+  return answer;
+}
