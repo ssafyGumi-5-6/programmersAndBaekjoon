cdde6e2fd0dcf7786ee2b612b1d23a25312c0629
이경창 <lkc263@naver.com>
2022-10-11 15:57:07 +0900

    ✏️ 2022.10.11 단속카메라 완료

--- 이경창/10.6~10.12/단속카메라/Solution.java
+++ 이경창/10.6~10.12/단속카메라/Solution.java
@@ -1,8 +1,10 @@
 package 단속카메라;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Comparator;
 
-    static int solution(int[][] routes){
+public class Solution {
+    static int solution(int[][] routes) {
         // 단속 카메라
         // - routes에는 차량의 이동 경로가 포함되어 있다.
         // - routes[i][0], routes[i][1]에는 i번째 차량이 진입 나간 지점이 적혀있다.
@@ -16,9 +18,34 @@ public class Solution {
         // - [-18, -13], [-14, -5] : 1
         // - [-5, -3], [-20, 15] : 1
         // => 답 : 2
-        
-        return 0;
+
+        Arrays.sort(routes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        // 0번 저장 도착지점 시작
+        int start = routes[0][1];
+        int answer = 1;
+        for(int i =1; i< routes.length; i++){
+            if(start < routes[i][0]){
+                answer +=1 ;
+                start = routes[i][1];
+            }
+        }
+
+        return answer;
     }
+
+
     public static void main(String[] args) {
+        int[][] arr = {{-20, 15}, {-14,-5}, {-18,-13}, {-5,-3}};
+
+        System.out.println(solution(arr));
+
+
+
     }
 }
