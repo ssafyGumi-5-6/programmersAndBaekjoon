6e10e5f7352aa740767a88bb796b317122e48a58
이경창 <lkc263@naver.com>
2022-11-13 12:14:09 +0900

    ✏️ 2022. 표편집 후보키

--- 이경창/10.27~11.3/표_편집/Solution.java
+++ 이경창/10.27~11.3/표_편집/Solution.java
@@ -1,2 +1,77 @@
-package 표_편집;public class Solution {
+package 표_편집;
+
+
+import java.util.*;
+
+class Solution {
+
+    int checkUDCZ(char c){
+        if(c == 'U'){
+            // U인 경우 위로 n만큼
+            return 1;
+        }else if(c == 'D'){
+            return 2;
+        }else if(c == 'C'){
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+
+    public String solution(int n, int k, String[] cmd) {
+        String answer = "";
+        boolean[] visited = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+
+        // U X : X칸 위에 있는 행을 선택
+        // D X : X칸 아래에 있는 행을 선택
+        // C : 현재 선택된 행 삭제 후, 바로 아래 행을 선택 (가장 마지막 행인 경우 바로 윗행을 선택)
+        // Z : 가장 최근에 삭제된 행을 원래대로 복구, 현재 선택된 행은 바뀌지 않음
+
+        for(int i = 0; i< cmd.length; i++){
+            String s = cmd[i];
+
+            if(checkUDCZ(s.charAt(0)) == 1){
+                String[] res = s.split(" ");
+                int cnt = Integer.parseInt(res[1]);
+                while(cnt > 0){
+                    cnt -= 1;
+                    if(visited[k]){
+                        cnt += 1;
+                    }
+                    k -= 1;
+                    if(k == -1) k = n-1;
+                }
+            }else if(checkUDCZ(s.charAt(0)) == 2){
+                String[] res = s.split(" ");
+                int cnt = Integer.parseInt(res[1]);
+                while(cnt > 0){
+                    cnt -= 1;
+                    if(visited[k]){
+                        cnt += 1;
+                    }
+                    k += 1;
+
+                    if(k == n) k = 0;
+                }
+            }else if(checkUDCZ(s.charAt(0)) == 3){
+                visited[k] = true;
+                stack.push(k);
+                k += 1;
+                if(k == n) k = 0;
+            }else if(checkUDCZ(s.charAt(0)) == 4){
+                if(stack.size() > 0){
+                    visited[stack.pop()] = true;
+                }
+            }
+
+            while(visited[k]){
+                k += 1;
+                if(k == n) k = 0;
+            }
+        }
+
+        return answer;
+    }
 }
+
--- 이경창/11.10~11.16/후보키/Solution.java
+++ 이경창/11.10~11.16/후보키/Solution.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    static int relLen;
+    static int answer;
+
+    // 중복한 개 있는지 확인하기
+    static boolean duplication(String[] dup){
+        Map<String, Integer> map = new HashMap<>();
+        for(String in_res : dup){
+            Integer in_value = map.get(in_res);
+//            System.out.println("in_res : " + in_res + " " + in_value);
+            if(in_value == null) map.put(in_res, 1);
+            else return false;
+        }
+
+        return true;
+    }
+
+    static void backtracking(int index, String indexStr,String[] res, Map<String, Integer> map, String[][] relation){
+
+        if(index == relation.length){
+            answer = Math.max(answer, map.size());
+            System.out.println("결과 res : " + res.toString() + " map : " + map  + " answer : " + answer );
+            return;
+        }
+
+        // 현재 구간 확인한다.
+        boolean check = duplication(relation[index]);
+
+        System.out.println("현재 index" + index + " check "  +check + "  " + indexStr);
+        // 만약 후보키라면 저장한다.
+        if(check){
+            backtracking(index + 1, indexStr, res, map, relation);
+            map.put(Integer.toString(index), 1);
+            backtracking(index + 1, indexStr, res, map, relation);
+
+        }else{
+            // 현재 index가 후보키가 아니라면 이전 현재 저장된 문자열에 추가
+            for(int i = 0; i < res.length; i++){
+                res[i] += relation[index][i];
+            }
+
+            // 합쳤을 때 후보키라면 res에 저장된거 제거하고 결과에 저장
+            boolean check2 = duplication(res);
+            if(check2){
+                indexStr += Integer.toString(index);
+                if(!map.containsKey(indexStr)) map.put(indexStr, 1);
+                backtracking(index + 1,"",new String[relLen], map, relation);
+            }else{
+                backtracking(index + 1, indexStr + index, res, map, relation);
+            }
+        }
+
+
+        // map에 넣을 때 get(String)이 null이라면 넣는다.
+
+    }
+
+    static int solution(String[][] relation) {
+        String[][] relationData = new String[relation[0].length][relation.length];
+        for(int i = 0; i < relation[0].length; i++){
+            for(int j =0; j< relation.length; j++){
+                relationData[i][j] = relation[j][i];
+            }
+        }
+
+        int answer = 0;
+        relLen = relationData[0].length;
+        backtracking(0, "" ,new String[relLen],new HashMap<>(), relationData );
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+//        String[][] inputString = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}
+//        };
+
+//        String[][] inputString = {{"a","aa"},{"aa","a"},{"a","a"}};
+//
+//        String[][] inputString = { {"a","1","aaa","c","ng"},
+//                {"a","1","bbb","e","g"},
+//                {"c","1","aaa","d","ng"},
+//                {"d","2","bbb","d","ng"}};
+//
+
+        String[][] inputString = {{"a","1","aaa","c","ng"},{"b","1","bbb","c","g"},{"c","1","aaa","d","ng"},{"d","2","bbb","d","ng"} };
+        solution(inputString);
+    }
 }
