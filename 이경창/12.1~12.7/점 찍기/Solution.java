59ba13066a9633182913c7a816387abdc6f211af
이경창 <lkc263@naver.com>
2022-12-03 01:25:40 +0900

    ✏️ 2022.12.3 점 찍기

---
+++ 이경창/12.1~12.7/점 찍기/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for (int i = 0; i * k <= d; i++) {
+            double y2 = Math.pow(d, 2) - Math.pow(i * k, 2);
+            answer += (int) (Math.sqrt(y2) / k) + 1;
+        }
+
+        return answer;
+    }
+}
