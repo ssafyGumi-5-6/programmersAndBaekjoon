1f7684da3fcae29c1f3d803aaafa1709f7b6b0fe
lkc263 <lkc263@naer.com>
2023-04-16 01:26:13 +0900

    연속된 부분 수열의 합

--- 이경창/2023/23.04.13~23.04.20/연속된부분수열의합/Solution.java
+++ 이경창/2023/23.04.13~23.04.20/연속된부분수열의합/Solution.java
@@ -1,2 +1,35 @@
-public class Solution {
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+
+        int startIdx = 0;
+        int sequenceSum = 0;
+        int curResultLen = 0;
+        int curLeftIdx = 0;
+        int curRightIdx = 0;
+
+
+        for(int i = 0; i < sequence.length; i++){
+            sequenceSum += sequence[i];
+
+            if(sequenceSum >= k){
+                while(sequenceSum > k){
+                    sequenceSum -= sequence[startIdx];
+                    startIdx += 1;
+                }
+
+                if(sequenceSum == k && curResultLen == 0 && curLeftIdx == 0){
+                    curResultLen = (i - startIdx) + 1;
+                    curLeftIdx = startIdx;
+                    curRightIdx = i;
+                }else if(sequenceSum == k && curResultLen > (i - startIdx) + 1){
+                    curResultLen = (i - startIdx) + 1;
+                    curLeftIdx = startIdx;
+                    curRightIdx = i;
+                }
+
+            }
+        }
+        int[] answer = {curLeftIdx, curRightIdx};
+        return answer;
+    }
 }
