4607e02387d0ea1a02616a87b24a556ccfe3b042
이경창 <lkc263@naver.com>
2023-06-05 23:34:03 +0900

    임스와 함께하는 미니게임

---
+++ 이경창/2023/23.06.01~23.06.07/임스와_함께하는_미니게임/Solution.kt
@@ -0,0 +1,27 @@
+package 임스와_함께하는_미니게임
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+class Solution {
+    fun main(args: Array<String>) {
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val st = StringTokenizer(br.readLine())
+        val N = st.nextToken().toInt()
+        val gameType = st.nextToken()
+        val list = ArrayList<String>()
+
+        for (i in 0 until N) {
+            list.add(br.readLine())
+        }
+
+        val set: Set<String> = HashSet(list)
+        val sb = StringBuilder()
+
+        if (gameType == "Y") sb.append(set.size)
+        else if (gameType == "F") sb.append(set.size / 2)
+        else if (gameType == "O") sb.append(set.size / 3)
+        println(sb.toString())
+    }
+}
