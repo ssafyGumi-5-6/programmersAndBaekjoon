25bf41e16d0cf7d802c9cd55ce0456f07a6f7e51
이경창 <lkc263@naver.com>
2023-06-13 08:08:57 +0900

    집합

--- 이경창/2023/23.06.01~23.06.07/임스와_함께하는_미니게임/Solution.kt
+++ 이경창/2023/23.06.01~23.06.07/임스와_함께하는_미니게임/Solution.kt
@@ -4,24 +4,24 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
 
-class Solution {
-    fun main(args: Array<String>) {
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        val st = StringTokenizer(br.readLine())
-        val N = st.nextToken().toInt()
-        val gameType = st.nextToken()
-        val list = ArrayList<String>()
-
-        for (i in 0 until N) {
-            list.add(br.readLine())
-        }
-
-        val set: Set<String> = HashSet(list)
-        val sb = StringBuilder()
-
-        if (gameType == "Y") sb.append(set.size)
-        else if (gameType == "F") sb.append(set.size / 2)
-        else if (gameType == "O") sb.append(set.size / 3)
-        println(sb.toString())
-    }
-}
+//class Solution {
+//    fun main(args: Array<String>) {
+//        val br = BufferedReader(InputStreamReader(System.`in`))
+//        val st = StringTokenizer(br.readLine())
+//        val N = st.nextToken().toInt()
+//        val gameType = st.nextToken()
+//        val list = ArrayList<String>()
+//
+//        for (i in 0 until N) {
+//            list.add(br.readLine())
+//        }
+//
+//        val set: Set<String> = HashSet(list)
+//        val sb = StringBuilder()
+//
+//        if (gameType == "Y") sb.append(set.size)
+//        else if (gameType == "F") sb.append(set.size / 2)
+//        else if (gameType == "O") sb.append(set.size / 3)
+//        println(sb.toString())
+//    }
+//}
--- 이경창/2023/23.06.08~23.06.14/집합/Solution.java
+++ 이경창/2023/23.06.08~23.06.14/집합/Solution.java
@@ -1,7 +1,46 @@
 package 집합;
 
+import java.io.*;
+import java.util.*;
+
 public class Solution {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int bit_set = 0;
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            int num;
+
+            switch (op) {
+                case "add":
+                    num = Integer.parseInt(st.nextToken());
+                    bit_set |= (1 << (num - 1));
+                    break;
+                case "remove":
+                    num = Integer.parseInt(st.nextToken());
+                    bit_set = bit_set & ~(1 << (num - 1));
+                    break;
+                case "check":
+                    num = Integer.parseInt(st.nextToken());
+                    sb.append((bit_set & (1 << (num - 1))) != 0 ? "1\n" : "0\n");
+                    break;
+                case "toggle":
+                    num = Integer.parseInt(st.nextToken());
+                    bit_set ^= (1 << (num - 1));
+                    break;
+                case "all":
+                    bit_set |= (~0);
+                    break;
+                case "empty":
+                    bit_set &= 0;
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+        br.close();
     }
 }
