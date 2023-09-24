70367453598aa903c792a2cb456b13e4ee1fce5b
이경창 <lkc263@naver.com>
2023-09-24 10:18:34 +0900

    숫자 정사각형

--- 이경창/2023/추천문제모음집/숫자_정사각형/Solution4.java
+++ 이경창/2023/추천문제모음집/숫자_정사각형/Solution4.java
@@ -15,29 +15,35 @@ public class Solution4 {
         else return false;
     }
 
+    // 정사각형
     private static boolean straight(int size, int x, int y){
-        System.out.println("size : " + size);
-        System.out.println("시작위치 : " + x + " " + y);
-        for(int[] inDxy : dxy1){
-            int nextY = inDxy[1] * size;
-            int nextX = inDxy[0] * size;
-            System.out.println(nextX + " " + nextY);
+//        System.out.println("정사각형 탐지, size : " + size);
+//        System.out.println("시작지점 : " + y + " " + x);
+        for(int[] inDxy : dxy2){
+            int nextX = inDxy[0] * size + x;
+            int nextY = inDxy[1] * size + y;
+
+//            System.out.println("nextX : " + nextX + " nextY : " + nextY);
             if(!withInRange(nextX, nextY)) return false;
             if(arr[y][x] != arr[nextY][nextX]) return false;
         }
         return true;
     }
 
-    private static boolean diagonally(int size, int x, int y) {
-        for (int[] inDxy : dxy2) {
-            int nextY = inDxy[1] * size;
-            int nextX = inDxy[0] * size;
-            System.out.println(nextX + " " + nextY);
-            if(!withInRange(nextX, nextY)) return false;
-            if (arr[y][x] != arr[nextY][nextX]) return false;
-        }
-        return true;
-    }
+
+    // 마름모
+//    private static boolean diagonally(int size, int x, int y) {
+////        System.out.println("마름모 탐지, size : " + size);
+//        for (int[] inDxy : dxy) {
+//            int nextX = inDxy[0] * size + x;
+//            int nextY = inDxy[1] * size + y;
+//
+////            System.out.println("nextX : " + nextX + " nextY : " + nextY);
+//            if(!withInRange(nextX, nextY)) return false;
+//            if (arr[y][x] != arr[nextY][nextX]) return false;
+//        }
+//        return true;
+//    }
 
         public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -57,12 +63,14 @@ public class Solution4 {
         int result = 1;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
+//                System.out.println("행 : " + i + " 열 : " + j);
                 for(int k = 1; k <= minData; k++){
-                    if(straight(k, j, i) || diagonally(k, j, i)){
-
-                        result = Math.max(result, k * k);
+//                    System.out.println("k : " + k);
+                    if(straight(k, j, i)){
+                        result = Math.max(result, (k + 1) * (k + 1));
                     }
                 }
+//                System.out.println();
             }
         }
 
