240398a8e7b7658c6a73e6ab4111bfe2627c7b8e
이경창 <lkc263@naver.com>
2023-10-04 16:59:52 +0900

    동전 1

---
+++ 이경창/2023/추천문제모음집/동전1/Solution2.java
@@ -0,0 +1,41 @@
+package 동전1;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution2 {
+
+    private static int n, k;
+    private static long[] dp;
+    private static int[] arr;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        k = Integer.parseInt(tokenizer.nextToken());
+
+        arr = new int[n];
+        dp = new long[k+1];
+
+        for(int i = 0 ; i < n; i++){
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+
+        Arrays.sort(arr);
+        dp[0] = 1;
+
+        for(int i = 0; i < n; i++){
+            for(int j = arr[i]; j <= k; j++){
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+
+        
+        System.out.println(dp[k]);
+
+
+        reader.close();
+    }
+}
