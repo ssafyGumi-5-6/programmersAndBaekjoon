b503c80f80128843f229bb96bfddf590b7b5a16e
이경창 <lkc263@naver.com>
2023-10-05 17:13:15 +0900

    숫자 재배치

---
+++ 이경창/2023/추천문제모음집/숫자_재배치/Solution2.java
@@ -0,0 +1,12 @@
+package 숫자_재배치;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        // backtracking
+
+        // A부터 시작해서 하나씩 줄여나간다.
+        // 만약, B보다 작다면 결과 값 비교
+        
+
+    }
+}
